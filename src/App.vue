92cecbde24bcc6a08fde2be8073d7de712715a9f
Jefferson <jeffersoneuclides84@gmail.com>
2020-11-13 10:23:17 -0300

    Stable version

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app>
+    
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      class="pt-2"
+      permanent
+      dark
+    >
+      <v-list>
+        <v-list-item
+          v-for="item in items"
+          :key=item.title
+          link
+          :to="item.path"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+    </v-navigation-drawer>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data () {
+      return {
+        items: [
+          { title: 'Alunos', icon: 'mdi-view-dashboard', path: '/students' }
+        ]
+      }
+    }
+};
+</script>
